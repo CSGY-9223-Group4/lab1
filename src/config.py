288da0dd6440e4dce0a21ef0c250a71b6a66a487
Brett Christiansen <wbc2025@nyu.edu>
2024-10-20 22:32:11 -0500

    bad formatting

--- src/config.py
+++ src/config.py
@@ -5,14 +5,27 @@ class Settings:
     # TODO: move secrets to using GitHub Secrets
     def __init__(self):
         # Database configurations
-        self.DB_HOST: str = os.getenv("DB_HOST")
-        self.DB_PORT: int = int(os.getenv("DB_PORT", "0"))
-        self.DB_USER: str = os.getenv("DB_USER")
-        self.DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-        self.DB_NAME: str = os.getenv("DB_NAME")
+        self.DB_HOST: str = os.getenv(
+            "DB_HOST"
+        )
+        self.DB_PORT: int = int(os.getenv(
+            "DB_PORT", 
+            "0"
+        ))
+        self.DB_USER: str = os.getenv(
+            "DB_USER"
+        )
+        self.DB_PASSWORD: str = os.getenv(
+            "DB_PASSWORD"
+        )
+        self.DB_NAME: str = os.getenv(
+            "DB_NAME"
+        )
 
         # Application configurations
-        self.JWT_SECRET_KEY: str = os.getenv("JWT_SECRET")
+        self.JWT_SECRET_KEY: str = os.getenv(
+            "JWT_SECRET"
+        )
 
 
 # Instantiate the settings
